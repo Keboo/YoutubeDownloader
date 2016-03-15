dd2813a174bc4e5784f44d077e990ab2a23672c6
Kevin Bost <kevin@intellitect.com>
2016-03-14 21:52:22 -0700

    Updating nugets Added missing files

---
+++ YoutubeDownloader/ViewModel/DownloadViewModel.cs
@@ -0,0 +1,188 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using FirstFloor.ModernUI.Windows.Controls;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Win32;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using YoutubeDownloader.Controls;
+using YoutubeExtractor;
+
+namespace YoutubeDownloader.ViewModel
+{
+    public class DownloadViewModel : ViewModelBase
+    {
+        private readonly IDialogService _DialogService;
+        private readonly ObservableCollection<VideoInfo> _Videos = new ObservableCollection<VideoInfo>();
+        private readonly RelayCommand<string> _FindVideoCommand;
+        private readonly RelayCommand<VideoInfo> _DownloadFileCommand;
+
+        public DownloadViewModel( IDialogService dialogService )
+        {
+            if ( dialogService == null ) throw new ArgumentNullException( "dialogService" );
+            _DialogService = dialogService;
+            _FindVideoCommand = new RelayCommand<string>( OnFindVideo, CanFindVideo );
+            _DownloadFileCommand = new RelayCommand<VideoInfo>( OnDownloadFile, CanDownloadFile );
+            try
+            {
+                if ( Clipboard.ContainsText() )
+                {
+                    var text = Clipboard.GetText();
+                    Uri uri;
+                    if ( Uri.TryCreate( text, UriKind.Absolute, out uri ) &&
+                        uri.Host.ToLowerInvariant().Contains( "youtube" ) )
+                    {
+                        _YoutubeUrl = uri.ToString();
+                    }
+                }
+            }
+            catch ( Exception )
+            { }
+        }
+
+        public ObservableCollection<VideoInfo> Videos
+        {
+            get { return _Videos; }
+        }
+
+        public ICommand FindVideoCommand
+        {
+            get { return _FindVideoCommand; }
+        }
+
+        public ICommand DownloadFileCommand
+        {
+            get { return _DownloadFileCommand; }
+        }
+
+        private string _YoutubeUrl;
+        public string YoutubeUrl
+        {
+            get { return _YoutubeUrl; }
+            set
+            {
+                if ( Set( ref _YoutubeUrl, value ) )
+                {
+                    _FindVideoCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        private string _Title;
+        public string Title
+        {
+            get { return _Title; }
+            set { Set( ref _Title, value ); }
+        }
+
+        private VideoInfo _SelectedVideo;
+        public VideoInfo SelectedVideo
+        {
+            get { return _SelectedVideo; }
+            set
+            {
+                if ( Set( ref _SelectedVideo, value ) )
+                {
+                    _DownloadFileCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        private bool _AudioOnly;
+        public bool AudioOnly
+        {
+            get { return _AudioOnly; }
+            set { Set( ref _AudioOnly, value ); }
+        }
+
+        private bool _IsDownloading;
+        public bool IsDownloading
+        {
+            get { return _IsDownloading; }
+            set { Set( ref _IsDownloading, value ); }
+        }
+
+        private void OnFindVideo( string url )
+        {
+            try
+            {
+                _Videos.Clear();
+                foreach ( var video in DownloadUrlResolver.GetDownloadUrls( url )
+                    .OrderByDescending( x => x.Resolution )
+                    .ThenByDescending( x => x.VideoType ) )
+                {
+                    _Videos.Add( video );
+                }
+                if ( _Videos.Count > 0 )
+                {
+                    Title = _Videos[0].Title;
+                }
+            }
+            catch ( Exception ex )
+            {
+                _DialogService.ShowError( ex, "Cannot Locate Video", "OK", null );
+            }
+        }
+
+        private bool CanFindVideo( string url )
+        {
+            return !string.IsNullOrWhiteSpace( url );
+        }
+
+        private bool CanDownloadFile( VideoInfo video )
+        {
+            return video != null;
+        }
+
+        private void OnDownloadFile( VideoInfo video )
+        {
+            string fileFilter = AudioOnly
+                ? "Audio|*" + video.AudioExtension
+                : "Video|*" + video.VideoExtension;
+            var saveFileDialog = new SaveFileDialog
+            {
+                Title = "Save File",
+                AddExtension = true,
+                OverwritePrompt = true,
+                RestoreDirectory = true,
+                Filter = fileFilter + "|All Files|*"
+            };
+
+            if ( saveFileDialog.ShowDialog() == true )
+            {
+                var downloadControl = new DownloadingControl();
+                var downloadingVM = downloadControl.ViewModel;
+                var dlg = new ModernDialog
+                {
+                    Title = "Downloading File",
+                    Content = downloadControl
+                };
+                dlg.OkButton.Content = "_done";
+                dlg.OkButton.SetBinding( UIElement.IsEnabledProperty,
+                    new Binding( "DownloadFinished" )
+                    {
+                        Source = downloadingVM
+                    } );
+                dlg.Buttons = new[] { dlg.OkButton, dlg.CancelButton };
+
+                if ( AudioOnly )
+                {
+                    downloadingVM.StartAudioDownload( video, saveFileDialog.FileName );
+                }
+                else
+                {
+                    downloadingVM.StartVideoDownload( video, saveFileDialog.FileName );
+                }
+                if ( dlg.ShowDialog() != true )
+                {
+                    downloadingVM.CancelDownload();
+                }
+            }
+        }
+    }
+}
---
+++ YoutubeDownloader/ViewModel/FileDownloadViewModel.cs
@@ -0,0 +1,84 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using YoutubeExtractor;
+
+namespace YoutubeDownloader.ViewModel
+{
+    public class FileDownloadViewModel : ViewModelBase
+    {
+        private CancellationTokenSource _TokenSource;
+
+        private string _Title;
+        public string Title
+        {
+            get { return _Title; }
+            set { Set( ref _Title, value ); }
+        }
+
+        private double _DownloadPercentage;
+        public double DownloadPercentage
+        {
+            get { return _DownloadPercentage; }
+            set { Set( ref _DownloadPercentage, value ); }
+        }
+
+        private bool _DownloadFinished;
+        public bool DownloadFinished
+        {
+            get { return _DownloadFinished; }
+            set { Set(ref _DownloadFinished, value); }
+        }
+
+        public void CancelDownload()
+        {
+            if ( _TokenSource != null )
+            {
+                _TokenSource.Cancel();
+            }
+        }
+
+        public void StartVideoDownload( VideoInfo video, string destinationFile )
+        {
+            if ( video == null ) throw new ArgumentNullException( "video" );
+            if ( destinationFile == null ) throw new ArgumentNullException( "destinationFile" );
+
+            var downloader = new VideoDownloader(video, destinationFile);
+            downloader.DownloadProgressChanged += OnDownloadProgressChanged;
+            StartDownload( downloader );
+        }
+
+        public void StartAudioDownload( VideoInfo video, string destinationFile )
+        {
+            if ( video == null ) throw new ArgumentNullException( "video" );
+            if ( destinationFile == null ) throw new ArgumentNullException( "destinationFile" );
+
+            var downloader = new AudioDownloader( video, destinationFile );
+            downloader.DownloadProgressChanged += OnDownloadProgressChanged;
+            StartDownload( downloader );
+        }
+
+        private void StartDownload( Downloader downloader )
+        {
+            if ( downloader.Video.RequiresDecryption )
+            {
+                DownloadUrlResolver.DecryptDownloadUrl( downloader.Video );
+            }
+            Title = downloader.Video.Title;
+
+            _TokenSource = new CancellationTokenSource();
+            downloader.DownloadFinished += (sender, e) => DownloadFinished = true;
+            Task.Run( () => downloader.Execute(), _TokenSource.Token );
+        }
+
+        private void OnDownloadProgressChanged( object sender, ProgressEventArgs e )
+        {
+            if ( _TokenSource != null && _TokenSource.IsCancellationRequested )
+            {
+                e.Cancel = true;
+            }
+            DownloadPercentage = e.ProgressPercentage;
+        }
+    }
+}
